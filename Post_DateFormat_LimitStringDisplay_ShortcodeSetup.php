625f46fa89f40bbc854c548a6361d16c68b41786
Ivan M. Dolera <ivandolera24@gmail.com>
2025-03-07 06:50:51 +0800

    Create Post - DateFormat - LimitStringDisplay - ShortcodeSetup
    Format Post Date
    Limit String Display
    Shortcode Default Setup - avoid buffering

---
+++ Post_DateFormat_LimitStringDisplay_ShortcodeSetup.php
@@ -0,0 +1,29 @@
+function getNewsDateFormat($data) {
+    // Febraury 25, 2025 format
+    $getdate = explode(' ', $data);
+    $getdate = date_create($getdate[0]);
+    $getdate =  date_format($getdate,"F d, Y");
+    return  $getdate;
+
+}
+
+function limitStringDisplay($string, $limit){
+    if(strlen($string) >= $limit) {
+         $limitedString =  substr($string, 0, $limit).'...';
+
+    } else {
+        $limitedString = $string;
+    }
+
+   // return $limitedString .'-'.strlen($string);
+   return $limitedString;
+}
+
+function readymadeshortcode($atts){ 
+    ob_start();
+    ob_end_clean(); // avoid buffering
+    $html = '';
+
+	return $html;
+
+}
