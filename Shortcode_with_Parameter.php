4502591d69cffe472edc7cdb9eef87a8680a5790
Ivan M. Dolera <ivandolera24@gmail.com>
2025-03-07 16:47:09 +0800

    Create Shortcode with Parameter

---
+++ Shortcode_with_Parameter.php
@@ -0,0 +1,12 @@
+function my_custom_shortcode($atts) {
+    // Set default values and extract shortcode attributes
+    $atts = shortcode_atts(array(
+        'name' => 'Guest',
+        'age' => 'unknown'
+    ), $atts, 'custom_shortcode');
+
+    return "Hello, my name is " . esc_html($atts['name']) . " and I am " . esc_html($atts['age']) . " years old.";
+}
+add_shortcode('custom_shortcode', 'my_custom_shortcode');
+
+[custom_shortcode name="John Doe" age="30"]
