12589120968304e38ebc6e827f3bef579b991fda
Ivan M. Dolera <ivandolera24@gmail.com>
2025-07-10 17:23:18 +0800

    Update OptimizeSpeedMobileDesktopPlugin.php

--- OptimizeSpeedMobileDesktopPlugin.php
+++ OptimizeSpeedMobileDesktopPlugin.php
@@ -8,7 +8,9 @@ Notes :
 2. Add this script on footer to hide the water mark
 3. Enable the gd extension in PHP settings.
 4. IF Jetpack is install simply go to ( Performance & speed- disabled accelarate )
-5. If litespeed plugin was installed, expect to have an issue. try to install again the litespeed and turn off the cache and purge all and install again the sepahinite accelerator plugin
+5. If litespeed plugin was installed, expect to have an issue. try to install again the litespeed and turn off the cache and purge all and install again the sepahinite
+accelerator plugin
+6. Incase not working, delete the (litespeed folder, advanced-cache.php and seraphinite-accelerator (inside cache folder) and begin start wizard in settings page
 <script> 
 	document.addEventListener("DOMContentLoaded", function() { 
 		var enlacesConImagen = document.querySelectorAll('a img\[alt="Seraphinite Accelerator"\]'); 
